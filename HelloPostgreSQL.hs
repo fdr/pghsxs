dba74de6f7a97b089c1f90aec0d71b6aab87ed1b
Dan Farina <drfarina@acm.org>
2010-06-02 01:10:54 -0700

    Add initial version
    Include pg_freespacemap for reference.
    
    Also, a very simple Haskell foreign export example.
    
    Signed-off-by: Dan Farina <drfarina@acm.org>

---
+++ HelloPostgreSQL.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+module HelloPostgres where
+
+foreign export ccall foo :: Int -> IO Int
+
+foo :: Int -> IO Int
+foo n = return (length (f n))
+
+f :: Int -> [Int]
+f 0 = []
+f n = n:(f (n-1))
