8587e9508e06efb4f0b9b5592ae026a4a3bea260
Dan Farina <drfarina@acm.org>
2010-07-03 22:59:19 -0700

    Add a test regular-C style postgres function, pg_finestructure
    Use this to try loading it:
    
    CREATE OR REPLACE FUNCTION pg_finestructure()
    RETURNS int2
    AS '!!ABSPATHHERE!!', 'pg_finestructure'
    LANGUAGE C STRICT;
    
    Signed-off-by: Dan Farina <drfarina@acm.org>

--- hsaux.c
+++ hsaux.c
@@ -3,3 +3,13 @@
 #include "funcapi.h"
 
 PG_MODULE_MAGIC;
+
+Datum		pg_finestructure(PG_FUNCTION_ARGS);
+
+PG_FUNCTION_INFO_V1(pg_finestructure);
+
+Datum
+pg_finestructure(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_INT32(137);
+}
