e32f8b9f4f39294a01e55333b8ff310242629f0a
Dan Farina <drfarina@acm.org>
2010-07-03 23:26:37 -0700

    hs_init hacking
    Signed-off-by: Dan Farina <drfarina@acm.org>

--- hsaux.c
+++ hsaux.c
@@ -2,11 +2,29 @@
 
 #include "funcapi.h"
 
+#include "HsFFI.h"
+
 PG_MODULE_MAGIC;
 
+#define __GLASGOW_HASKELL__
+
 Datum		pg_finestructure(PG_FUNCTION_ARGS);
+Datum		pg_hs_init(PG_FUNCTION_ARGS)
 
 PG_FUNCTION_INFO_V1(pg_finestructure);
+PG_FUNCTION_INFO_V1(pg_hs_init);
+
+Datum
+pg_hs_init(PG_FUNCTION_ARGS)
+{
+	hs_init(NULL, NULL);
+
+#ifdef __GLASGOW_HASKELL__
+	hs_add_root(__stginit_HS42);
+#endif
+
+	PG_RETURN_NULL();
+}
 
 Datum
 pg_finestructure(PG_FUNCTION_ARGS)
