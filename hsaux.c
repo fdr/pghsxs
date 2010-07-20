cae39e275fd346b3dad171e879786d8d6c093e5a
Dan Farina <drfarina@acm.org>
2010-07-19 20:10:10 -0700

    Utilize Postgres library-load hook
    Correct hs_init call. It would reference unallocated memory before.
    
    Most nicely, now there is no need for the bizarre pg_hs_init() hack
    from before; the first time a function in the shared library is
    touched, hs_init gets called For Free.
    
    Signed-off-by: Dan Farina <drfarina@acm.org>

--- hsaux.c
+++ hsaux.c
@@ -9,25 +9,25 @@ PG_MODULE_MAGIC;
 #define __GLASGOW_HASKELL__
 
 Datum		pg_finestructure(PG_FUNCTION_ARGS);
-Datum		pg_hs_init(PG_FUNCTION_ARGS);
 
 PG_FUNCTION_INFO_V1(pg_finestructure);
-PG_FUNCTION_INFO_V1(pg_hs_init);
 
 extern void __stginit_HS42(void);
+extern void _PG_init(void);
 
-Datum
-pg_hs_init(PG_FUNCTION_ARGS)
+void
+_PG_init(void)
 {
-	int argc = 0;
-	char *argv[] = {};
-	hs_init(&argc, &argv);
+	int argc = 1;
+	char *name = "pghsxs";
+	char *argv[1] = {name};
+	char **argv_alias = argv;
+
+	hs_init(&argc, &argv_alias);
 
 #ifdef __GLASGOW_HASKELL__
 	hs_add_root(__stginit_HS42);
 #endif
-
-	PG_RETURN_NULL();
 }
 
 Datum
